b76b8ea266d21c96c2c077bdc42fe1e8fc28733d
Xiaoting Ma <u7433915@anu.edu.au>
2024-06-24 16:02:10 +1000

    Ajusted layout detail

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
     <section id="about" className="about-section p-5">
       <Container>
         <Row>
-          <Col md={7}>
+          <Col md={6}>
             <h1>Hello I am</h1>
             <h2>Xiaoting Ma</h2>
             <h3>Web Developer & Software Developer</h3>
@@ -18,8 +18,8 @@ const About = () => {
 
             <Button variant="outline-primary" href="#contact">Contact Me</Button>
           </Col>
-          <Col md={5}>
-            <img src="profile2.jpg" alt="Xiaoting Ma's profile" className="img-fluid rounded-circle profile-img" />
+          <Col md={6}>
+            <img src="profile3.jpg" alt="Xiaoting Ma's profile" className="img-fluid rounded-circle profile-img" />
           </Col>
         </Row>
       </Container>
