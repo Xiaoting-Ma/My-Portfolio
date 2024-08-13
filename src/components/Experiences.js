1900555c9c42e7bd06ed05d32a5c891ee4e2f7e8
Xiaoting Ma <u7433915@anu.edu.au>
2024-08-14 09:10:52 +1000

    Updated Latest Experience

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -5,16 +5,22 @@ import '../index.css';
 const Experiences = () => {
   const experiences = [
     {
-      dateRange: "2019 - 2022",
-      title: "Customer Service Associate",
-      subtitle: "Canberra, Australia", 
-      description: ["Provided customer service to 1,000+ visitors daily, promptly addressing inquiries and resolving issues, achieving a high level of customer satisfaction. ", "Improved product selection and store displays by analyzing customer feedback, contributing to continuous improvement initiatives. ", "Streamlined processes to improve efficiency and service quality, resulting in better understanding and fulfillment of customer needs."]
+      dateRange: "2024",
+      title: "Youth Coding Lead Instructor",
+      subtitle: "OZ Code Academy - Canberra, Australia", 
+      description: ["Implemented interactive coding workshops using languages such as Python and JavaScript to foster problem-solving skills among students. ", "Supported the setup and maintenance of digital learning tools and environments, ensuring smooth operation and accessibility for all participants. ", "Collaborated with instructors to refine and optimize the curriculum based on real-time feedback, enhancing the educational impact of programming concepts."]
     },
     {
       dateRange: "2014 - 2018",
-      title: "Telecommunication Engineer and Cost Manager",
-      subtitle: "Xining, China", 
-      description: ["Developed and implemented IT support guidelines. Managed daily IT desktop and meeting support tasks, ensuring smooth operation of internal meeting systems and office software accounts. ", "Optimized internal maintenance processes through automation to increase efficiency and quality. ", "Integrated internal systems for network data sharing and interaction, developing interfaces for engineers and ensuring data security and consistency. ", "Earned Employee of the Year award and received a promotion within two years. "]
+      title: "Telecommunication IT Engineer and Cost Manager",
+      subtitle: "China Unicom - Xining, China", 
+      description: ["Managed departmental costs and budget for 7 key projects in a team of 100+ engineers, resulting in optimized resource allocation.", "Prepared plans for installations and upgrades of wireless networks in telecommunication systems.", "Developed and implemented IT support guidelines. Managed daily IT desktop and meeting support tasks, ensuring smooth operation of internal meeting systems and office software accounts. ", "Optimized internal maintenance processes through automation to increase efficiency and quality. ", "Integrated internal systems for network data sharing, developing interfaces for engineers and ensuring data security and consistency. ", "Earned Employee of the Year award and received a promotion within two years. "]
+    }, 
+    {
+      dateRange: "2019 - 2022",
+      title: "Customer Service Associate",
+      subtitle: "Retail Store - Canberra, Australia", 
+      description: ["Provided customer service to 1,000+ visitors daily, promptly addressing inquiries and resolving issues, achieving a high level of customer satisfaction. ", "Improved product selection and store displays by analyzing customer feedback, contributing to continuous improvement initiatives. ", "Streamlined processes to improve efficiency and service quality, resulting in better understanding and fulfillment of customer needs."]
     }
   ];
 
